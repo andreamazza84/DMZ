03cd127cd9f66cb3c8359ef6a38a50003de8de23
andreamazza84 <andrea.mazza84@gmail.com>
2021-08-17 09:32:27 +0200

    path bis

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,5 @@ module.exports = {
   ],
   outputDir: 'docs',
 
-  // publicPath: process.env.NODE_ENV === 'production' ? '/' : '/dmz/docs/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/docs' : '/'
 }
